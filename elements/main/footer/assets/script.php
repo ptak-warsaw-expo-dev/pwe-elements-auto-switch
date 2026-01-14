fbcf6f2c91b6ef82919196ad52639b61d70e0667
ptak-warsaw-expo-dev <68947073+ptak-warsaw-expo-dev@users.noreply.github.com>
2026-01-14 13:42:48 +0100

    Add files via upload

--- elements/main/footer/assets/script.php
+++ elements/main/footer/assets/script.php
@@ -10,17 +10,17 @@ $output .= '
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         const uncodeNavMenu = document.querySelector("#masthead");
-        const pweNavMenu = document.querySelector("#pweMenu");
+        const pweNavMenu = document.querySelector("#pweMenuAutoSwitch");
 
         // Top main menu "For exhibitors"
-        const mainMenu = pweNavMenu ? document.querySelector(".pwe-menu__nav") : document.querySelector("ul.menu-primary-inner");
+        const mainMenu = pweNavMenu ? document.querySelector(".pwe-menu-auto-switch__nav") : document.querySelector("ul.menu-primary-inner");
         const secondChild = mainMenu.children[1];
-        const dropMenu = pweNavMenu ? secondChild.querySelector(".pwe-menu__submenu") : secondChild.querySelector("ul.drop-menu");
+        const dropMenu = pweNavMenu ? secondChild.querySelector(".pwe-menu-auto-switch__submenu") : secondChild.querySelector("ul.drop-menu");
 
         // Create new element li
         const newMenuItem = document.createElement("li");
         newMenuItem.id = pweNavMenu ? "" : "menu-item-99999";
-        newMenuItem.className = pweNavMenu ? "pwe-menu__submenu-item" : "menu-item menu-item-type-custom menu-item-object-custom menu-item-99999";
+        newMenuItem.className = pweNavMenu ? "pwe-menu-auto-switch__submenu-item" : "menu-item menu-item-type-custom menu-item-object-custom menu-item-99999";
         newMenuItem.innerHTML = `<a title="'. (get_locale() == "pl_PL" ? 'Zostań agentem' : 'Become an agent') .'" target="_blank" href="https://warsawexpo.eu'. (get_locale() == "pl_PL" ? '/formularz-dla-agentow/' : '/en/forms-for-agents/') .'">'. (get_locale() == "pl_PL" ? 'Zostań agentem' : 'Become an agent') .'</a>`;
 
         // Add new element as second in the list
@@ -36,7 +36,7 @@ $output .= '
 
             // Create new element li
             const instructionMenuItem = document.createElement("li");
-            instructionMenuItem.className = pweNavMenu ? "pwe-menu__submenu-item" : "menu-item menu-item-type-custom menu-item-object-custom menu-item-99999";
+            instructionMenuItem.className = pweNavMenu ? "pwe-menu-auto-switch__submenu-item" : "menu-item menu-item-type-custom menu-item-object-custom menu-item-99999";
             instructionMenuItem.innerHTML = `<a title="'. (get_locale() == "pl_PL" ? 'Instrukcja aplikacji' : 'Application instructions') .'" target="_blank" href="https://warsawexpo.eu'. (get_locale() == "pl_PL" ? '/docs/Instrukcja-do-aplikacji.pdf' : '/docs/Instrukcja-do-aplikacji.pdf') .'">'. (get_locale() == "pl_PL" ? 'Instrukcja aplikacji' : 'Application instructions') .'</a>`;
 
             // Add new element as penultimate in the list
