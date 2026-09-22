9792609a5990786d09b47cae77b0d7e846704b73
Anton Melnychuk <87388822+antonmelnychuk1@users.noreply.github.com>
2026-09-22 12:44:43 +0200

    Delete assets directory

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    const langInput = document.querySelector(".lang input");
-    if (langInput) {
-        const pageLang = (document.documentElement.lang || "").trim().toLowerCase();
-        langInput.value = pageLang ? pageLang.substring(0, 2) : "";
-    }
-}
